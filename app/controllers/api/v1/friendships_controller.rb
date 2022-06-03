4212af87d67942688977c93dc1f8b635f4054c64
ashleylovette <ashleylovette15@gmail.com>
2022-06-03 14:03:49 -0500

    authenticate

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -3,7 +3,6 @@
 module Api
   module V1
     class FriendshipsController < Api::V1::ApplicationController
-      skip_before_action :authenticate
       def add_friend
         result= BaseApi::FriendshipService.save_friend(params[:friend_id], params[:current_user_id])
         render_error(errors: "Could not add #{result.payload.email} as a friend", status: 401) and return unless result.success?
